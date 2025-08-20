c1302f6cbdc70f695666859c6750b5e9c21b5734
Aeneas Meier <aeneas.meier@stud.unibas.ch>
2025-08-20 06:18:27 +0000

    initial run script

---
+++ experiments/2025-08-19-initial-tests/run.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3
+
+import os
+from pathlib import Path
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+
+import common_setup
+from common_setup import IssueConfig, IssueExperiment
+
+
+# ARCHIVE_PATH = "ai/downward/issue1186"
+REPO_DIR = Path("/home/aeneas/Git/downward-projects/downward").expanduser()
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+BUILDS = ["release"] # "release" or "debug"
+# REVISIONS = ["main", "tiebreaking-num-ops"]
+REVISIONS = ["tiebreaking-num-ops"]
+CONFIG_NICKS = [
+    # ("astar-blind", ["--search", "astar(blind())"]),
+    ("astar-hmax", ["--search", "astar(lmcut(pcfstrategy=hmax))"]),
+    ("astar-hmaxtie", ["--search", "astar(lmcut(pcfstrategy=hmaxtie))"]),
+]
+CONFIGS = [
+    IssueConfig(
+        config_nick, config, build_options=[build], driver_options=["--build", build]
+    )
+    for build in BUILDS
+    for config_nick, config in CONFIG_NICKS
+]
+
+SUITE = common_setup.DEFAULT_OPTIMAL_SUITE
+
+ENVIRONMENT = BaselSlurmEnvironment(
+    partition="infai_2",
+    email="aeneas.meier@stud.unibas.ch",
+    #memory_per_cpu="3947M",
+    export=["PATH"],
+)
+
+if common_setup.is_test_run():
+    SUITE = IssueExperiment.DEFAULT_TEST_SUITE
+    ENVIRONMENT = LocalEnvironment(processes=4)
+
+exp = IssueExperiment(
+    REPO_DIR,
+    revisions=REVISIONS,
+    configs=CONFIGS,
+    environment=ENVIRONMENT,
+)
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(exp.PLANNER_PARSER)
+
+exp.add_step("build", exp.build)
+exp.add_step("start", exp.start_runs)
+exp.add_step("parse", exp.parse)
+exp.add_fetcher(name="fetch")
+
+SPECIAL_ATTRIBUTES = []
+ATTRIBUTES = exp.DEFAULT_TABLE_ATTRIBUTES + SPECIAL_ATTRIBUTES
+SCATTER_ATTRIBUTES = SPECIAL_ATTRIBUTES
+
+exp.add_absolute_report_step(attributes=ATTRIBUTES)
+exp.add_comparison_table_step(attributes=ATTRIBUTES)
+exp.add_scatter_plot_step(relative=False, attributes=["search_time", "expansions", "evaluations"])
+
+# exp.add_archive_step(ARCHIVE_PATH)
+# exp.add_archive_eval_dir_step(ARCHIVE_PATH)
+
+exp.run_steps()
